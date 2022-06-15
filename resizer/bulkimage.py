b2075175d0cee7fcb60ab426b381cd832d0c4a9a
VenuAswin <106885733+VenuAswin@users.noreply.github.com>
2022-06-15 21:42:48 +0530

    Fix intendation

--- resizer/bulkimage.py
+++ resizer/bulkimage.py
@@ -3,20 +3,20 @@ import PIL
 import os
 
 def resiz():
+	
 	images = [file for file in os.listdir() if file.endswith(('jpeg', 'png', 'jpg'))]
+	
 	for image in images:
-	    img = Image.open(image)
-
-	fixed_height=420
-	height_percent = (fixed_height / float(img.size[1]))
-	width_size = int((float(img.size[0]) * float(height_percent)))
-
-	# resize 
-	resized = img.resize((width_size,fixed_height), PIL.Image.NEAREST)
-	print(resized.size)
-
-	# Shows the image in image viewer
-	resized.show()
-	resized.save("resized_" + image, optimize=True, quality=40)
+		
+		img = Image.open(image)
+		fixed_height=420
+		height_percent = (fixed_height / float(img.size[1]))
+		width_size = int((float(img.size[0]) * float(height_percent)))
+		# resize
+		resized = img.resize((width_size,fixed_height), PIL.Image.NEAREST)
+		print(resized.size)
+		# Shows the image in image viewer
+		resized.show()
+		resized.save("resized_" + image, optimize=True, quality=40)
 
 resiz()
